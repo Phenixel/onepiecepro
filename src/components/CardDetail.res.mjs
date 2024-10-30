d91fc3c55cd221519fe5d02ca1d10a922ee5c0be
Phenixel <yonathancardoso@outlook.fr>
2024-10-30 12:37:43 +0100

    fix: Meilleur affichage de la carte avec fond sombre

--- src/components/CardDetail.res.mjs
+++ src/components/CardDetail.res.mjs
@@ -15,40 +15,52 @@ function CardDetail(props) {
   var isExpanded = match[0];
   return JsxRuntime.jsxs("div", {
               children: [
-                JsxRuntime.jsx("div", {
-                      children: JsxRuntime.jsx("img", {
-                            className: "w-full",
-                            alt: card.name,
-                            src: card.imageUrl
-                          }),
-                      className: "card-image"
-                    }),
-                isExpanded ? JsxRuntime.jsxs("div", {
-                        children: [
-                          JsxRuntime.jsx("h2", {
-                                children: card.name
-                              }),
-                          JsxRuntime.jsx("p", {
-                                children: "Cost: " + String(card.cost)
-                              }),
-                          JsxRuntime.jsx("p", {
-                                children: "Power: " + String(card.power)
-                              }),
-                          JsxRuntime.jsx("p", {
-                                children: "Effect: " + card.effect
-                              })
-                        ],
-                        className: "card-details"
-                      }) : null
-              ],
-              className: "card-container cursor-pointer " + (
-                isExpanded ? "expanded" : "collapsed"
-              ),
-              onClick: (function (param) {
-                  setIsExpanded(function (prev) {
-                        return !prev;
-                      });
-                })
+                isExpanded ? JsxRuntime.jsx("div", {
+                        className: "overlay visible",
+                        onClick: (function (param) {
+                            setIsExpanded(function (param) {
+                                  return false;
+                                });
+                          })
+                      }) : null,
+                JsxRuntime.jsxs("div", {
+                      children: [
+                        JsxRuntime.jsx("div", {
+                              children: JsxRuntime.jsx("img", {
+                                    className: "w-full",
+                                    alt: card.name,
+                                    src: card.imageUrl
+                                  }),
+                              className: "card-image"
+                            }),
+                        isExpanded ? JsxRuntime.jsxs("div", {
+                                children: [
+                                  JsxRuntime.jsx("h2", {
+                                        children: card.name
+                                      }),
+                                  JsxRuntime.jsx("p", {
+                                        children: "Cost: " + String(card.cost)
+                                      }),
+                                  JsxRuntime.jsx("p", {
+                                        children: "Power: " + String(card.power)
+                                      }),
+                                  JsxRuntime.jsx("p", {
+                                        children: "Effect: " + card.effect
+                                      })
+                                ],
+                                className: "card-details"
+                              }) : null
+                      ],
+                      className: "card-container cursor-pointer " + (
+                        isExpanded ? "expanded" : "collapsed"
+                      ),
+                      onClick: (function (param) {
+                          setIsExpanded(function (prev) {
+                                return !prev;
+                              });
+                        })
+                    })
+              ]
             });
 }
 
